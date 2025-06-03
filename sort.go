a3cdc4f78285d5c3e3d2f0c0414a7ae24bea2b3e
niansq <niansq@gmail.com>
2025-06-03 18:23:08 +0800

    新增排序算法

--- sort.go
+++ sort.go
@@ -9,3 +9,13 @@ func SelectSort(nums []int){
 		}
 	}
 }
+
+func BubbleSort(nums []int){
+	for i:=0;i<len(nums);i++{
+		for j:=0;j<len(nums)-i-1;j++{
+			if nums[j]<nums[j+1]{
+				nums[j],nums[j+1] = nums[j+1],nums[j]
+			}
+		}
+	}
+}
